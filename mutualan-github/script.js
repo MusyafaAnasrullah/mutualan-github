b61c3b657ea8701627f9254ec39c9f81acec955f
MusyafaAnasrullah <musyafaanasrullah4@gmail.com>
2026-01-08 23:14:49 +0700

    perfect

--- mutualan-github/script.js
+++ mutualan-github/script.js
@@ -1,18 +1,40 @@
-const users = [
-  { username: "octocat", name: "GitHub Mascot" }
+const members = [
+    {
+        username: "MusyafaAnasrullah",
+        name: "Musyafa Anasrullah",
+        avatar: "https://github.com/musyafaanasrullah.png"
+    },
+    {
+        username: "Ahmad Hibrizi",
+        name: "hazetprojects",
+        avatar: "https://github.com/hazetprojects.png"
+    },
+    {
+        username: "Felicity",
+        name: "felicityblueish",
+        avatar: "https://github.com/felicityblueish.png"
+    },
+    {
+        username: "Aymen Berbiche",
+        name: "waamara",
+        avatar: "https://github.com/waamara.png"
+    }
 ];
 
-const container = document.getElementById("listUser");
+const container = document.getElementById("cardContainer");
 
-users.forEach(user => {
-  container.innerHTML += `
-    <div class="card">
-      <img src="https://github.com/${user.username}.png">
-      <div class="username">${user.username}</div>
-      <div>${user.name}</div>
-      <a href="https://github.com/${user.username}" target="_blank">
-        <button>Lihat Profil</button>
-      </a>
-    </div>
-  `;
+members.forEach(member => {
+    const card = document.createElement("div");
+    card.className = "card";
+
+    card.innerHTML = `
+        <img src="${member.avatar}" alt="${member.username}">
+        <div class="username">${member.username}</div>
+        <div class="name">${member.name}</div>
+        <a href="https://github.com/${member.username}" target="_blank">
+            Lihat Profil
+        </a>
+    `;
+
+    container.appendChild(card);
 });
